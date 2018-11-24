6032caa1ea7ebfe4c664f9b5fa7ab8df23c5f71c
Student <student@Redmi4-GLaDOSPhone.localdomain>
2018-11-24 13:15:42 +0200

    add build

---
+++ build/precache-manifest.02a1fe1b9dc4e158ca18bdbba2cde9bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "40a211f0b6515499b6add6894f8190db",
+    "url": "/static/media/background.40a211f0.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "755cb410731a9072c68e",
+    "url": "/static/js/main.755cb410.chunk.js"
+  },
+  {
+    "revision": "3a1759330c1867161d00",
+    "url": "/static/js/1.3a175933.chunk.js"
+  },
+  {
+    "revision": "755cb410731a9072c68e",
+    "url": "/static/css/main.6963329a.chunk.css"
+  },
+  {
+    "revision": "cc5f159d2512a758a84fa265447e08f0",
+    "url": "/index.html"
+  }
+];
